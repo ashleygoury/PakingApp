0119fc6da41789190cc41439d73f4d7bfdd47c9e
Ashley Goury <ashleygoury@outlook.com>
2020-02-13 12:03:46 -0500

    methodOfNotification

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -24,7 +24,6 @@ import {setInterval} from "tns-core-modules/timer";
 import {HttpClient} from "@angular/common/http";
 import {LocalNotifications} from "nativescript-local-notifications";
 import * as Toast from 'nativescript-toast';
-import {not} from "rxjs/internal-compatibility";
 
 
 registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
@@ -173,13 +172,13 @@ export class MapComponent implements OnInit, DoCheck {
         this.cfalertDialog.show(options);
     }
 
-    // notifyTime(minStart: number, date: Date, notifyMe: Date, timeStart: number) {
-    //     if (minStart === 0) {
-    //         notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart - 1, minStart + 30);
-    //     } else if (minStart === 30) {
-    //         notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart);
-    //     }
-    // }
+    notifyTime(minStart: number, date: Date, timeStart: number) {
+        if (minStart === 0) {
+            return new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart - 1, minStart + 30);
+        } else if (minStart === 30) {
+            return new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart);
+        }
+    }
 
     showBottomSheet(lat, lng, fullYear, timeStart, minStart, timeEnd, minEnd, timeTwoStart, minTwoStart, timeTwoEnd, minTwoEnd, days, i): void {
         let date = new Date();
@@ -194,17 +193,9 @@ export class MapComponent implements OnInit, DoCheck {
             for (let i = 0; i < days.length; i++) {
                 if (date.getDay() === days[i].Day) {
                     if (date.getHours() < timeStart) {
-                        if (minStart === 0) {
-                            notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart - 1, minStart + 30);
-                        } else if (minStart === 30) {
-                            notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart);
-                        }
+                        notifyMe = this.notifyTime(minStart, date, timeStart);
                     } else if (date.getHours() > timeEnd && date.getMinutes() > minEnd && timeTwoStart !== 0 && date.getHours() < timeTwoStart) {
-                        if (minStart === 0) {
-                            notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeTwoStart - 1, minTwoStart + 30);
-                        } else if (minStart === 30) {
-                            notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeTwoStart);
-                        }
+                        notifyMe = this.notifyTime(minTwoStart, date, timeTwoStart);
                     }
                 }
             }
@@ -247,13 +238,8 @@ export class MapComponent implements OnInit, DoCheck {
                 //Add the current date
                 date.setDate(date.getDate() + difference);
 
-                if (minStart === 0) {
-                    notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart - 1, minStart + 30);
-                } else if (minStart === 30) {
-                    notifyMe = new Date(date.getFullYear(), date.getMonth(), date.getDate(), timeStart);
-                }
+                notifyMe = this.notifyTime(minStart, date, timeStart);
             }
-            Toast.makeText("Outside with num 3 day " + notifyMe).show();
         }
 
         const notify = response => {
